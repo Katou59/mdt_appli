51e8f9a8100b01ee72d9de667878ad158c6a390f
Katou <catteau.alexandre5988@gmail.com>
2025-09-26 17:16:22 +0200

    next auth

--- mdt_appli/app/page.tsx
+++ mdt_appli/app/page.tsx
@@ -1,20 +1,15 @@
 import Image from "next/image";
+import {signIn} from "@/auth";
 
 export default function Home() {
     return (
-        <div className="card bg-base-100 w-96 shadow-sm">
-            <figure>
-                <img
-                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                    alt="Shoes"/>
-            </figure>
-            <div className="card-body">
-                <h2 className="card-title">Card Title</h2>
-                <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
-                </div>
-            </div>
-        </div>
+        <form
+            action={async () => {
+                "use server"
+                await signIn("discord")
+            }}
+        >
+            <button type="submit" className="btn btn-primary">Signin with Discord</button>
+        </form>
     );
 }
