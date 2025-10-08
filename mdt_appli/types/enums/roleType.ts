027bd326696f6d7fb43c658000be6b547e0ed399
Alexandre Catteau <katou@MacBook-Pro-dAlexandre.local>
2025-10-08 18:30:01 +0200

    corr

--- mdt_appli/types/enums/roleType.ts
+++ mdt_appli/types/enums/roleType.ts
@@ -1,5 +1,5 @@
 // ⚙️ Fichier généré automatiquement — ne pas modifier à la main
-// Dernière génération : 2025-10-08T16:12:09.990Z
+// Dernière génération : 2025-10-08T16:19:22.521Z
 
 export enum RoleType {
     User = 0,
