ffd38a1c4bd5203a3fca17bc414b49e586523ace
Katou <catteau.alexandre5988@gmail.com>
2025-10-11 16:15:02 +0200

    error eslint

--- mdt_appli/auth.ts
+++ mdt_appli/auth.ts
@@ -1,18 +1,24 @@
-import * as NextAuth from 'next-auth';
+import NextAuth from "next-auth";
+import DiscordProvider from "next-auth/providers/discord";
+import type { JWT } from "next-auth/jwt";
+import type { Session, User } from "next-auth";
+import { Account, Profile } from "@auth/core/types";
 
-import Discord from "next-auth/providers/discord";
-
-export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [Discord],
+export const authOptions = {
+    providers: [DiscordProvider],
     callbacks: {
-        async jwt({ token, profile, account }) {
+        async jwt({ token, profile, account }: { token: JWT; profile?: Profile; account?: Account }) {
             if (profile?.id) token.discordId = profile.id;
             if (account?.providerAccountId) token.discordId = account.providerAccountId;
             return token;
         },
-        async session({ session, token }) {
-            if (token.discordId) (session.user as any).discordId = token.discordId as string;
+        async session({ session, token }: { session: Session; token: JWT }) {
+            if (token.discordId) {
+                (session.user as User & { discordId?: string }).discordId = token.discordId as string;
+            }
             return session;
         },
     },
-});
+};
+
+export const { handlers, signIn, signOut, auth } = NextAuth(authOptions);
