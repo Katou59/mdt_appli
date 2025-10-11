3760d81cfcc2d77af81fcb77bd03fa99d45b0f4b
Katou <catteau.alexandre5988@gmail.com>
2025-10-11 14:57:16 +0200

    changement fav ico

--- mdt_appli/types/enums/roleType.ts
+++ mdt_appli/types/enums/roleType.ts
@@ -1,5 +1,5 @@
 // ⚙️ Fichier généré automatiquement — ne pas modifier à la main
-// Dernière génération : 2025-10-10T22:58:22.540Z
+// Dernière génération : 2025-10-11T12:53:25.571Z
 
 export enum RoleType {
     User = 0,
--- mdt_appli/types/enums/statusType.ts
+++ mdt_appli/types/enums/statusType.ts
@@ -1,5 +1,5 @@
 // ⚙️ Fichier généré automatiquement — ne pas modifier à la main
-// Dernière génération : 2025-10-10T22:58:23.582Z
+// Dernière génération : 2025-10-11T12:53:26.876Z
 
 export enum StatusType {
     En_vie = 1,
