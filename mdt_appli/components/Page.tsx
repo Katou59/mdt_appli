fe4dd9acd10c2c643c7005dbf116f1515353523e
Katou <catteau.alexandre5988@gmail.com>
2025-10-21 21:37:07 +0200

    footer

--- mdt_appli/components/Page.tsx
+++ mdt_appli/components/Page.tsx
@@ -12,7 +12,7 @@ export default function Page({ children, title }: Props) {
                 <h1 className="text-4xl font-bold text-center my-4">{title}</h1>
             </header>
             <main className="flex flex-col items-center grow">{children}</main>
-            <footer className="mt-8 pt-4 text-center text-sm text-gray-500 grid gap-2">
+            <footer className="mt-8 pt-4 text-center text-sm text-primary-foreground/50 grid gap-2">
                 <Separator/>
                 © 2025 MDT Application — Développé par Katou
             </footer>
